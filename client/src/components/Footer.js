a3178bbd1973fd8f86af40ca9aff6ba61aef754d
Goodness Ezeokafor <gootech442@yahoo.com>
2020-02-27 23:04:52 +0100

    newchanges

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer= () => {
                 <div class="col-12 col-sm-6 col-lg-4">
                     <div class="single-footer">
                         <div class="logo-area footer">
-                        <a href="#" style={{fontSize:"25px", color:"white"}}>ARAGON</a>                        </div>
+                        <a href="#" style={{fontSize:"25px", color:"white"}}>ARAGONTech</a>                        </div>
                         <div class="space-20"></div>
                         <p>
                             Using deep technology to build viable products
